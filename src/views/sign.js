5bda3c677b6deab7226df869dd42abd124dab60e
Leonard Pak <leopak2000@gmail.com>
2022-10-05 01:34:09 +0300

    [DEV] fix sign.js

--- src/views/sign.js
+++ src/views/sign.js
@@ -3,14 +3,13 @@
  * @module sign
  */
 
-import processForm from "../modules/validationForm.js";
+import { processForm, formType } from "../modules/validationForm.js";
 
 /**
  * @const {Object} contextSignUp объект с контекстом страницы регистрации
  */
 const contextSignUp = {
   formTitle: 'Регистрация',
-  formName: 'signup',
   inputs: [
     {
       title: 'Логин',
@@ -52,20 +51,19 @@ const contextSignUp = {
 export default async (router) => {
   router.root.innerHTML = '';
 
-  const form = Handlebars.templates.form;
-  const el = document.createElement('div');
-  el.id = 'main';
-  el.className = 'main';
+  const signlog = Handlebars.templates.signlog;
+  const main = document.createElement('div');
+  main.className = 'main';
   const frm = document.createElement('form');
   frm.className = 'form';
-  frm.name = formName;
-  frm.onsubmit =() => {
-    processForm(frm, router); 
+  frm.name = formType.signup;
+  frm.onsubmit = () => {
+    processForm(frm, router);
     return false;
   };
-  frm.innerHTML += form(contextSignUp);
-  el.appendChild(frm)
-  router.root.appendChild(el);
+  frm.innerHTML += signlog(contextSignUp);
+  main.appendChild(frm)
+  router.root.appendChild(main);
 
   const footer = Handlebars.templates.footer;
   router.root.innerHTML += footer();
