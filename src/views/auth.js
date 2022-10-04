d34dc5ca44c493d91b637277c411d8cc734d0409
Leonard Pak <leopak2000@gmail.com>
2022-10-04 23:03:21 +0300

    [DEV] fix auth view

--- src/views/auth.js
+++ src/views/auth.js
@@ -32,17 +32,10 @@ const contextAuth = {
  * @param {Router} router Класс маршрутизации по страницам сайта
  */
 export default async (router) => {
-  const params = new URL(location.href).searchParams;
-  const id = params.get('id');
-  const header = Handlebars.templates.header;
   router.root.innerHTML = '';
 
-  const form = Handlebars.templates.form;
-  const el = document.createElement('div');
-  el.id = 'main';
-  el.className = 'main';
-  el.innerHTML += form(contextAuth);
-  router.root.appendChild(el);
+  const signlog = Handlebars.templates.signlog;
+  router.root.innerHTML += signlog(contextAuth);
 
   const footer = Handlebars.templates.footer;
   router.root.innerHTML += footer();
