0fd906446eb296d1d0963218a06b1240891ba230
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-10-07 13:33:15 +0300

    [VD-44] fix logout

--- src/views/logout.js
+++ src/views/logout.js
@@ -8,6 +8,7 @@
  * @param {Router} router Класс маршрутизации по страницам сайта
  */
 export default (router) => {
+  router.id = undefined;
   router.api.logout()
       .then(router.goTo('/login'));
 };
