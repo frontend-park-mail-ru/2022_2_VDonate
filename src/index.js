6ca842333827b1f78be78de55a57e37d9af150ce
Leonard Pak <leopak2000@gmail.com>
2022-10-19 14:05:04 +0300

    [VD-52] add button, add ts configurattion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 'use_strict';
 
-import App from '@modules/app';
-import './style.styl';
+// import App from '@modules/app';
+// import './style.styl';
+import {Button, ButtonType} from '@components/button/button.ts';
 
 /**
  * Основной класс веб-приложения
  * @const {App} app
  */
-const app = new App();
-app.authUser();
+// const app = new App();
+// app.authUser();
+
+const but = new Button(ButtonType.primary, 'Нажми меня!', 'submit');
+document.getElementById('main')?.appendChild(but.element);
