fb7fdef99546e1734249e1412cbcbe80ceaa3dac
Leonard Pak <leopak2000@gmail.com>
2022-10-23 18:35:15 +0300

    [VD-86] fix combineReducer

--- src/modules/flux/combineReducer.ts
+++ src/modules/flux/combineReducer.ts
@@ -2,16 +2,20 @@ import {IAction} from './types/actions';
 import {Reducer} from './types/reducer';
 import {State} from './types/store';
 
+/**
+ * Функция комбинирования нескольких редьюсеров в один обобщенный
+ * @param reducers - ассоциативный массив редьюсеров
+ * @returns обобщенный редьюсер
+ */
 const combineReducers =
   (reducers: Record<string, Reducer>): Reducer =>
-    (store: State, action: IAction): State => {
+    (state: State, action: IAction): State => {
       Object.entries(reducers).forEach(
           ([key, reducer]) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            store[key] = reducer(store[key], action);
+            state[key] = reducer(state[key] as State, action);
           },
       );
-      return store;
+      return state;
     };
 
 export default combineReducers;
