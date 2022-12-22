ce5d4710e9d99899391fc68b33088b07d70215b7
Loen15 <59060743+Loen15@users.noreply.github.com>
2022-12-22 21:32:51 +0300

    [dev] redesign buttons

--- src/components/ProfileInfo/ProfileInfo.ts
+++ src/components/ProfileInfo/ProfileInfo.ts
@@ -6,6 +6,7 @@ import Button, {ButtonType} from '@components/Button/Button';
 import {becomeAuthor} from '@actions/handlers/user';
 import {openProfileEditor, openWithdrawEditor} from '@actions/handlers/editor';
 import {notice} from '@actions/handlers/notice';
+import MiniStatistic from '@components/MiniStatistic/MiniStatistic';
 
 interface ProfileInfoOptions {
   isAuthor: boolean
@@ -40,11 +41,7 @@ export default
 class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
   private avatar!: Avatar;
   private username!: HTMLSpanElement;
-  private countSubscriptions!: HTMLSpanElement;
-  private countDonaters!: HTMLSpanElement;
-  private countPosts!: HTMLSpanElement;
-  private countProfitMounth!: HTMLSpanElement;
-  private countSubscribersMounth!: HTMLSpanElement;
+  private miniStatistic!: MiniStatistic;
 
   constructor(el: HTMLElement, private options: ProfileInfoOptions) {
     super();
@@ -61,10 +58,6 @@ class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
 
     if (!this.options.isAuthor && data.isAuthor) {
       this.options.isAuthor = data.isAuthor;
-      this.countSubscriptions.parentElement?.remove();
-      this.renderAuthorStatistic(
-          querySelectorWithThrow(this.domElement, '.mini-statistic'),
-      );
       if (this.options.changeable) {
         querySelectorWithThrow(this.domElement, '.profile-info__become-author')
             .remove();
@@ -74,13 +67,6 @@ class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
       }
     } else if (this.options.isAuthor && !data.isAuthor) {
       this.options.isAuthor = false;
-      this.countDonaters.parentElement?.remove();
-      this.countPosts.parentElement?.remove();
-      this.countProfitMounth.parentElement?.remove();
-      this.countSubscribersMounth.parentElement?.remove();
-      this.renderDonaterStatistic(
-          querySelectorWithThrow(this.domElement, '.mini-statistic'),
-      );
       if (this.options.changeable) {
         new Button(
             querySelectorWithThrow(this.domElement, '.profile-info'),
@@ -96,39 +82,14 @@ class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
       }
     }
 
-    if (this.options.isAuthor) {
-      if (this.options.countDonaters !== data.countDonaters) {
-        this.options.countDonaters = data.countDonaters;
-        this.countDonaters.innerText = data.countDonaters.toString();
-      }
-      if (this.options.countPosts !== data.countPosts) {
-        this.options.countPosts = data.countPosts;
-        this.countPosts.innerText = data.countPosts.toString();
-      }
-      if (this.options.changeable &&
-          this.options.countProfitMounth !== data.countProfitMounth) {
-        this.options.countProfitMounth = data.countProfitMounth;
-        this.countProfitMounth.innerHTML =
-          data.countProfitMounth.toString() + '&#8381;';
-      }
-      if (this.options.changeable &&
-        this.options.countSubscribersMounth !== data.countSubscribersMounth) {
-        this.options.countSubscribersMounth = data.countSubscribersMounth;
-        this.countSubscribersMounth.innerText =
-          data.countSubscribersMounth.toString();
-      }
-      if (this.options.changeable &&
-        this.options.balance !== data.balance) {
-        this.options.balance = data.balance;
-        querySelectorWithThrow(this.domElement, '.balance-area__balance-span')
-            .innerHTML = `Баланс ${this.options.balance}&#8381;`;
-      }
-    } else {
-      if (this.options.countSubscriptions !== data.countSubscriptions) {
-        this.options.countSubscriptions = data.countSubscriptions;
-        this.countSubscriptions.innerText = data.countSubscriptions.toString();
-      }
-    }
+    this.miniStatistic.update({
+      countSubscriptions: data.countSubscriptions,
+      countDonaters: data.countDonaters,
+      countPosts: data.countPosts,
+      countProfitMounth: data.countProfitMounth,
+      countSubscribersMounth: data.countSubscribersMounth,
+      isAuthor: data.isAuthor,
+    });
   }
 
   protected render(): HTMLDivElement {
@@ -152,18 +113,26 @@ class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
     const info = document.createElement('div');
     info.classList.add('profile-info__info-area');
 
-    const miniStatistic = document.createElement('div');
-    miniStatistic.classList.add('mini-statistic');
-    info.appendChild(miniStatistic);
     back.append(this.username, info);
 
     if (this.options.isAuthor) {
-      this.renderAuthorStatistic(miniStatistic);
+      this.miniStatistic = new MiniStatistic(info, {
+        countDonaters: this.options.countDonaters,
+        countPosts: this.options.countPosts,
+        countProfitMounth: this.options.countProfitMounth,
+        countSubscribersMounth: this.options.countSubscribersMounth,
+        isAuthor: true,
+        changeable: this.options.changeable,
+      });
       if (this.options.changeable) {
         this.renderBalance(back);
       }
     } else {
-      this.renderDonaterStatistic(miniStatistic);
+      this.miniStatistic = new MiniStatistic(info, {
+        countSubscriptions: this.options.countSubscriptions,
+        isAuthor: false,
+        changeable: this.options.changeable,
+      });
       if (this.options.changeable) {
         const becomeAuthorBtn = new Button(back, {
           viewType: ButtonType.PRIMARY,
@@ -213,78 +182,5 @@ class ProfileInfo extends ComponentBase<'div', ProfileInfoUpdateContext> {
       },
     }).addClassNames('balance-area__withdraw');
     div.appendChild(balanceArea);
-  }
-  private renderDonaterStatistic(miniStatistic: HTMLElement) {
-    const subsContainer = document.createElement('div');
-    subsContainer.classList.add('mini-statistic__container');
-
-    const subscriptionsTitle = document.createElement('span');
-    subscriptionsTitle.classList.add('mini-statistic__text', 'font_regular');
-    subscriptionsTitle.innerText = 'Подписок';
-
-    this.countSubscriptions = document.createElement('span');
-    this.countSubscriptions.classList
-        .add('mini-statistic__text', 'font_regular');
-    this.countSubscriptions.innerText =
-      this.options.countSubscriptions.toString();
-
-    subsContainer.append(subscriptionsTitle, this.countSubscriptions);
-    miniStatistic.appendChild(subsContainer);
-  }
-
-  private renderAuthorStatistic(miniStatistic: HTMLElement) {
-    const donatersContainer = document.createElement('div');
-    donatersContainer.classList
-        .add('mini-statistic__container');
-    const donaters = document.createElement('span');
-    donaters.classList.add('mini-statistic__text', 'font_regular');
-    donaters.innerText = 'Донатеров';
-    this.countDonaters = document.createElement('span');
-    this.countDonaters.classList.add('mini-statistic__text', 'font_regular');
-    this.countDonaters.innerText =
-      this.options.countDonaters?.toString() ?? '0';
-    donatersContainer.append(donaters, this.countDonaters);
-    miniStatistic.appendChild(donatersContainer);
-    const postsContainer = document.createElement('div');
-    postsContainer.classList
-        .add('mini-statistic__container');
-    const posts = document.createElement('span');
-    posts.classList.add('mini-statistic__text', 'font_regular');
-    posts.innerText = 'Постов';
-    this.countPosts = document.createElement('span');
-    this.countPosts.classList.add('mini-statistic__text', 'font_regular');
-    this.countPosts.innerText =
-      this.options.countPosts?.toString() ?? '0';
-    postsContainer.append(posts, this.countPosts);
-    miniStatistic.appendChild(postsContainer);
-    if (this.options.changeable) {
-      const profitContainer = document.createElement('div');
-      profitContainer.classList
-          .add('mini-statistic__container');
-      const profit = document.createElement('span');
-      profit.classList.add('mini-statistic__text', 'font_regular');
-      profit.innerText = 'Заработок за месяц';
-      this.countProfitMounth = document.createElement('span');
-      this.countProfitMounth
-          .classList.add('mini-statistic__text', 'font_regular');
-      this.countProfitMounth.innerText =
-      this.options.countProfitMounth?.toString() ?? '0';
-      this.countProfitMounth.innerHTML += '&#8381;';
-      profitContainer.append(profit, this.countProfitMounth);
-      miniStatistic.appendChild(profitContainer);
-      const subPerMounthContainer = document.createElement('div');
-      subPerMounthContainer.classList
-          .add('mini-statistic__container');
-      const subPerMounth = document.createElement('span');
-      subPerMounth.classList.add('mini-statistic__text', 'font_regular');
-      subPerMounth.innerText = 'Донатеров за месяц';
-      this.countSubscribersMounth = document.createElement('span');
-      this.countSubscribersMounth
-          .classList.add('mini-statistic__text', 'font_regular');
-      this.countSubscribersMounth.innerText =
-      this.options.countSubscribersMounth?.toString() ?? '0';
-      subPerMounthContainer.append(subPerMounth, this.countSubscribersMounth);
-      miniStatistic.appendChild(subPerMounthContainer);
-    }
   }
 }
